e464d6c4b3b42e81d03323caa584887a6fbf3624
Jacotheron <jaco@xilix.technology>
2026-08-12 22:15:20 +0200

    Various changes

--- resources/views/components/livewire/people/⚡person/person.blade.php
+++ resources/views/components/livewire/people/⚡person/person.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="flex flex-col justify-end rounded-sm bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 dark:text-neutral-200 min-w-48">
+    class="flex flex-col justify-end rounded-sm bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 dark:text-neutral-200 min-w-40">
     {{-- image --}}
     <div class="p-2">
         <a href="/people/{{ $person->id }}" title="{{ __('app.show_profile') }}">
